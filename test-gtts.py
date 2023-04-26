dd3d9d2a7fb0c7aaaa40c16121c2f314d973f0a9
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-04-26 23:28:16 +0800

    plat sound directly

--- test-gtts.py
+++ test-gtts.py
@@ -2,6 +2,7 @@ from gtts import gTTS
 from pydub.playback import play
 from pydub import AudioSegment
 
+from io import BytesIO
 # import subprocess
 
 def play_tts(pgd, pga):
@@ -18,4 +19,20 @@ def play_tts(pgd, pga):
     print('Unable to play', filename, e)
   # subprocess.call(["ffmpeg", "-i", filename])
 
-play_tts(1, 2)
+def play_tts_no_save():
+  text = "I love myself, I am thankful to be alive right now"
+  speech = gTTS(text=text, lang="en", slow=True)
+  # Convert the audio data to an in-memory file-like object
+  audio_file = BytesIO()
+  speech.write_to_fp(audio_file)
+  audio_file.seek(0)
+
+  # Load the audio data into pydub
+  audio_data = AudioSegment.from_file(audio_file, format='mp3')
+
+  # Play the audio
+  play(audio_data)
+
+# play_tts(1, 2)
+
+play_tts_no_save()
