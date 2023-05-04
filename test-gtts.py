66166bda20a7f47b7d27afad2658cce14867e63d
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-05-04 22:50:05 +0800

    combine audio

--- test-gtts.py
+++ test-gtts.py
@@ -1,9 +1,9 @@
 from gtts import gTTS
 from pydub.playback import play
-from pydub import AudioSegment
+from pydub import AudioSegment, utils
 
 from io import BytesIO
-# import subprocess
+import subprocess
 
 def play_tts(pgd, pga):
   text = ("Peak displacement is %.2f meters" \
@@ -33,6 +33,32 @@ def play_tts_no_save():
   # Play the audio
   play(audio_data)
 
+def play_combined_audio():
+  # sound 1
+  sound1 = AudioSegment.from_file("./eq_displacement.mp3", format="mp3")
+
+  # sound 2
+  text = "this is the second audio"
+  speech = gTTS(text=text, lang="en", slow=True)
+  # Convert the audio data to an in-memory file-like object
+  audio_file = BytesIO()
+  speech.write_to_fp(audio_file)
+  audio_file.seek(0)
+
+  # Load the audio data into pydub
+  sound2 = AudioSegment.from_file(audio_file, format='mp3')
+
+  final_audio = sound1 + sound2
+  final_audio_path = './final-audio.wav'
+  # play(final_audio)
+  final_audio.export(final_audio_path, format="wav")
+  PLAYER = utils.get_player_name()
+  subprocess.call([PLAYER,"-nodisp", "-autoexit", "-hide_banner", final_audio_path], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+  # subprocess.run(['play', '-q', '-t', 'wav', '-'], input=final_audio_path, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+
 # play_tts(1, 2)
 
-play_tts_no_save()
+# play_tts_no_save()
+
+play_combined_audio()
