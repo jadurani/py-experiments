869643f5445ffe7b0b474298bf89d908490734f3
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-06-26 14:14:47 +0800

    refactor

--- plt-relay-switch-ui.py
+++ plt-relay-switch-ui.py
@@ -7,6 +7,16 @@ rcParams['toolbar'] = 'None'
 use('Qt5Agg')
 plt.ion()
 
+# border_states
+CLOSED_YELLOW = 'closed-yellow'
+CLOSED_RED = 'closed-red'
+OPEN = 'open'
+
+IMAGE_LOCS = {
+  'closed-yellow': './images/relay-closed-yellow.png',
+  'closed-red': './images/relay-closed-red.png',
+  'open': './images/relay-open.png',
+}
 
 def _set_up_axes(ax):
   ax.xaxis.set_tick_params(labelbottom=False)
@@ -23,25 +33,31 @@ def _set_up_axes(ax):
   ax.spines['left'].set_visible(False)
   ax.spines['bottom'].set_visible(False)
 
+def _show_border(ax, border_state):
+  # Load the SVG image using mpimg
+  image_loc = IMAGE_LOCS[border_state]
+  image = mpimg.imread(image_loc)
 
-# Load the SVG image using mpimg
-image = mpimg.imread('./images/relay-closed-yellow.png')
+  # Calculate the desired width and height for resizing
+  desired_width = 100  # Set your desired width in pixels
+  aspect_ratio = image.shape[1] / image.shape[0]
+  desired_height = desired_width / aspect_ratio
 
-# Create a figure and axes
-fig = plt.figure(facecolor='#EEEEEE')
-ax = fig.add_subplot(212)
-ax.set_facecolor('#EEEEEE')
-# [left, bottom, width, height]
-ax.set_position([0.25, 0, 0.5, 0.5])
-_set_up_axes(ax)
+  # Plot the resized image on the axes
+  ax.imshow(image, interpolation='bilinear', extent=(0, desired_width, 0, desired_height))
 
-# Calculate the desired width and height for resizing
-desired_width = 100  # Set your desired width in pixels
-aspect_ratio = image.shape[1] / image.shape[0]
-desired_height = desired_width / aspect_ratio
 
-# Plot the resized image on the axes
-ax.imshow(image, interpolation='bilinear', extent=(0, desired_width, 0, desired_height))
+def show_figure():
+  # Create a figure and axes
+  fig = plt.figure(facecolor='#EEEEEE')
+  ax = fig.add_subplot(212)
+  ax.set_facecolor('#EEEEEE')
+  # [left, bottom, width, height]
+  ax.set_position([0.25, 0, 0.5, 0.5])
+  _set_up_axes(ax)
+  _show_border(ax, OPEN)
 
-# Show the plot
-fig.canvas.start_event_loop(5)
+  # Show the plot
+  fig.canvas.start_event_loop(5)
+
+show_figure()
